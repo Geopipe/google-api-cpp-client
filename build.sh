063a474c53c92bc5ec287b30af3b8f52fcfc60f0
Santa Zhang <santa1987@gmail.com>
2016-11-29 21:51:37 -0500

    minor update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 OS_UNAME=`uname`
 
@@ -11,42 +11,7 @@ else
     exit 1
 fi
 
-# http://stackoverflow.com/a/21951256/1035246
-abspath() {
-    local thePath
-    if [[ ! "$1" =~ ^/ ]]; then
-        thePath="$PWD/$1"
-    else
-        thePath="$1"
-    fi
-    echo "$thePath" | (
-        IFS=/
-        read -a parr
-        declare -a outp
-        for i in "${parr[@]}"; do
-            case "$i" in
-                ''|.)
-                    continue
-                    ;;
-                ..)
-                    len=${#outp[@]}
-                    if ((len==0)); then
-                        continue
-                    else
-                        unset outp[$((len-1))]
-                    fi
-                    ;;
-                *)
-                    len=${#outp[@]}
-                    outp[$len]="$i"
-                    ;;
-            esac
-        done
-        echo /"${outp[*]}"
-    )
-}
-
-ROOT=$(abspath $(dirname $0))
+ROOT=$(dirname $0)
 
 set -x
 
